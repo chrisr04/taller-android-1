8be4471b34cbda246ee5b1fdba10abacd2b53d17
Christian Rodriguez <christian971204@gmail.com>
2021-04-01 00:01:29 -0500

    Punto 9

--- app/src/main/java/com/example/tallerunimaguno/HomeActivity.java
+++ app/src/main/java/com/example/tallerunimaguno/HomeActivity.java
@@ -20,7 +20,6 @@ import android.widget.Toast;
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button matematicas;
-    Button texto;
     Button fisica; // A
     ImageButton help, home, logout;
 
@@ -32,14 +31,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         fisica = findViewById(R.id.btnPhysic); // A
         help = findViewById(R.id.btnHelp);
         home = findViewById(R.id.btnHome);
-        texto = findViewById(R.id.btnText);
         logout = findViewById(R.id.btnLogout);
         matematicas.setOnClickListener(this);
         fisica.setOnClickListener(this); // A
         help.setOnClickListener(this);
         home.setOnClickListener(this);
         logout.setOnClickListener(this);
-        texto.setOnClickListener(this);
     }
 
     @Override
@@ -65,10 +62,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btnHelp:
                 showHelpAlert();
-                break;
-            case R.id.btnText:
-                Intent k = new Intent(getApplicationContext(),TextActivity.class);
-                startActivity(k);
                 break;
         }
     }
